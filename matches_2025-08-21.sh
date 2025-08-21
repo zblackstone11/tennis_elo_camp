6ddbd98d82db6bacd3fffce87764984bf029bf6e
Zack Blackstone <zblackstone11@yahoo.com>
2025-08-21 18:37:22 -0400

    8/21 results

---
+++ matches_2025-08-21.sh
@@ -0,0 +1,11 @@
+# 8/21 Match Results
+
+python3 elo_camp.py record_series_singles Jack Devin 6-4
+python3 elo_camp.py record_series_singles Cooper Rishaan 1-6
+python3 elo_camp.py record_series_singles Jack Cadie 1-6
+python3 elo_camp.py record_series_singles Devin Harvey 3-6
+
+python3 elo_camp.py record_series_doubles Cadie Harvey Rhys Spencer 7-6
+python3 elo_camp.py record_series_doubles Spencer Rhys Rishaan Cooper 3-3
+python3 elo_camp.py record_series_doubles Spencer Rhys Rishaan Cooper "7-5[tiebreak]"
+python3 elo_camp.py record_series_doubles Harvey Jack Cadie Devin "10-8[tiebreak]"
